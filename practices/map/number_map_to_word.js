b664e1a3a2001f436f18dd88327232bb30e1b4ae
Yang Jie <jiyang@thoughtworks.com>
2018-09-04 23:04:01 +0800

    finish map

--- practices/map/number_map_to_word.js
+++ practices/map/number_map_to_word.js
@@ -1,6 +1,6 @@
 'use strict';
 var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+  return collection.map(a=>String.fromCharCode(a+0x60));
 };
 
 module.exports = number_map_to_word;
--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -1,6 +1,22 @@
 'use strict';
 var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+  return collection.map(a=>intToChar(a));
 };
+
+function intToChar(numberChar){
+  var decade = numberChar/26;
+  var unit = numberChar%26;
+  var result="";
+  if(decade>0){
+    result+=String.fromCharCode(decade+0x60);
+  }
+  if(unit>0){
+    result+=String.fromCharCode(numberChar%26+0x60);
+  }
+
+  return result;
+}
+
+
 
 module.exports = number_map_to_word_over_26;
